57306b7a554a6608bf9cfc14c9cb2d6a95b08ec5
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-11-25 21:44:38 -0300

    atualizando a lista de produtos

---
+++ js/productList.js
@@ -0,0 +1,56 @@
+var productList = [
+    {
+    "id": 1,
+    "title": "Capitão América",
+    "img": "captain_america.png",
+    "price": 230.00,
+    },
+    {
+    "id": 2,
+    "title": "Gremlin",
+    "img": "gremlin.png",
+    "price": 200.00,
+    },
+    {
+    "id": 3,
+    "title": "Groot",
+    "img": "groot.png",
+    "price": 270.00,
+    },
+    {
+    "id": 4,
+    "title": "Harry Potter",
+    "img": "harry_potter.png",
+    "price": 700.00,
+    },
+    {
+    "id": 5,
+    "title": "Homem de Ferro",
+    "img": "iron_man.png",
+    "price": 240.00,
+    },
+    {
+    "id": 6,
+    "title": "Pennywise",
+    "img": "pennywise.png",
+    "price": 200.00,
+    },
+    {
+    "id": 7,
+    "title": "Tardis",
+    "img": "tardis.png",
+    "price": 200.00,
+    },
+    {
+    "id": 8,
+    "title": "Voldemort",
+    "img": "voldemort.png",
+    "price": 800.00,
+    },
+    {
+    "id": 9,
+    "title": "Xenomorph",
+    "img": "xenomorph.png",
+    "price": 800.00,
+    }
+]
